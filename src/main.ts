9c72de0c673d75deb0d71bd7107db6b610034cb8
Chris Verbree <v@vmac.ch>
2024-03-06 14:07:25 +0100

    Fixed another label

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { App, Notice, Plugin, TAbstractFile, TFile, TFolder, addIcon, normalizeP
 import { DEFAULT_SETTINGS, NoteCompanionFolderPluginSettingTab, NoteCompanionFolderPluginSettings } from './settings';
 
 
-const LABEL = 'Reveal Companion Folder';
+const LABEL = 'Reveal companion folder';
 const ICON_ID = 'companion-folder';
 const ICON_SVG = '<g fill="none"><path d="M 9.92126 99 L 79.3701 99 C 84.84946 99 89.29136 95.7707 89.29136 90.29134 L 89.29136 28.283464 L 62.00789 1 L 19.842523 1 C 14.36316 1 9.92126 5.441899 9.92126 10.92126 L 9.92126 25.80315" stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="10"/><path d="M 59.52756 2.8346457 L 59.52756 32.598425 L 89.29134 32.598425" stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="10"/><path transform="translate(4, 0)" d="M 44.66635 84.70457 C 47.407297 84.70457 49.629275 82.48259 49.629275 79.74164 L 49.629275 54.927004 C 49.629275 52.186054 47.407297 49.964076 44.66635 49.964076 L 25.062784 49.964076 C 23.376214 49.98061 21.796687 49.139443 20.86911 47.73076 L 18.859125 44.753 C 17.941175 43.359114 16.384079 42.51996 14.71508 42.519685 L 4.9629275 42.519685 C 2.2219783 42.519685 -7105427e-21 44.741663 -7105427e-21 47.48261 L -7105427e-21 79.74164 C -7105427e-21 82.48259 2.2219783 84.70457 4.9629275 84.70457 Z" stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="8"/></g>';
 
